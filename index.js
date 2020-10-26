ca1dd79cf95e35d5ccb8f0b55839a0ae812b3d59
csh928 <csh928@gmail.com>
2020-10-26 19:36:50 +0900

    토큰 추가

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const config = require("./config/key");
 
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json 분석할수 있게
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -39,6 +41,42 @@ app.post("/register", (req, res) => {
       success: true,
     }); // 성공했을때
   }); //몽고디비 메서드
+});
+
+app.post("/api/users/login", (req, res) => {
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+
+  //몽고디비 메서드 findOne
+  User.findOne({ email: req.body.email }, (err, user) => {
+    //유저가 데이터베이스 없을때
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      //비밀번호 맞지 않을때
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      //비밀번호까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        //토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
+        res
+          .cookie("x-auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
 });
 
 app.listen(port, () => console.log(`Exaple app listening on port ${port}`));
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -30,6 +32,52 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+const saltRounds = 10;
+
+// 몽구스에서 가져온 데이터 저장하기 전에 할거
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    //패스워드 데이터가 바뀔때만
+    //비밀번호 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      //솔트생성
+      if (err) return next(err); //에러발생
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        //암호화된 비번
+        // Store hash in your password DB.
+        if (err) return next(err); //에러발생
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainPassword 1234567     암호화된 비밀번호 비교
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  //jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
